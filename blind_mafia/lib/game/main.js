6c382c0df8c9de9fb74d30d53504889af88d02bb
Franco Pellegrini <frapell@gmail.com>
2013-01-26 00:31:26 -0300

    experimentacion

--- blind_mafia/lib/game/main.js
+++ blind_mafia/lib/game/main.js
@@ -3,7 +3,9 @@ ig.module(
 )
 .requires(
 	'impact.game',
-	'impact.font'
+	'impact.font',
+    
+    'game.entities.shooteable'
 )
 .defines(function(){
 
@@ -15,6 +17,16 @@ MyGame = ig.Game.extend({
 	
 	init: function() {
 		// Initialize your game here; bind keys etc.
+        var png = "";
+        for( var y = 0; y < 4; y++ ) { 
+            if (y%2 == 0){
+                png = "red_square.png";
+            }
+            else{
+                png = "blue_square.png";
+            }
+            ig.game.spawnEntity( EntityShooteable, 100*y, 100*y, {png:png} );
+        }
 	},
 	
 	update: function() {
@@ -28,7 +40,6 @@ MyGame = ig.Game.extend({
 		// Draw all entities and backgroundMaps
 		this.parent();
 		
-		
 		// Add your own drawing code here
 		var x = ig.system.width/2,
 			y = ig.system.height/2;
@@ -38,8 +49,8 @@ MyGame = ig.Game.extend({
 });
 
 
-// Start the Game with 60fps, a resolution of 320x240, scaled
-// up by a factor of 2
-ig.main( '#canvas', MyGame, 60, 320, 240, 2 );
+// Start the Game with 60fps, a resolution of 853x480, scaled
+// up by a factor of 1
+ig.main( '#canvas', MyGame, 60, 853, 480, 1 );
 
 });
