86ab2c3af7ec8588a73507be05b48294888913fc
Franco Pellegrini <frapell@gmail.com>
2013-01-26 10:42:38 -0300

    Moviendo a un metodo propio

--- blind_mafia/lib/game/levels/manager.js
+++ blind_mafia/lib/game/levels/manager.js
@@ -17,7 +17,7 @@ ig.LevelsManager = ig.Class.extend({
         this.levels[name] = new level;
     },
     
-    load: function( name ){
+    clean_scene: function () {
         // Clean the Scene and start over
         ig.game.screen = {x: 0, y: 0};
 
@@ -27,8 +27,11 @@ ig.LevelsManager = ig.Class.extend({
         
         // Map Layer
         ig.game.collisionMap = ig.CollisionMap.staticNoCollision;
-        ig.game.backgroundMaps = [];
-
+        ig.game.backgroundMaps = [];    
+    },
+    
+    load: function( name ){
+        this.clean_scene();
         this.current = this.levels[name];
         this.current.ready();
     },
